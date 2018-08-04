26c2aa1e4349c964fd35095d11e15834f0ebf226
KingJA <kingjavip@gmail.com>
2018-08-04 13:18:46 +0800

    添加二维码显示

--- app/src/main/java/com/kingja/qiang/page/order/orderdetail/OrderDetailActivity.java
+++ app/src/main/java/com/kingja/qiang/page/order/orderdetail/OrderDetailActivity.java
@@ -3,19 +3,23 @@ package com.kingja.qiang.page.order.orderdetail;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.kingja.qiang.R;
+import com.kingja.qiang.adapter.QcodePagerAdapter;
 import com.kingja.qiang.base.BaseTitleActivity;
 import com.kingja.qiang.constant.Status;
-import com.kingja.qiang.imgaeloader.ImageLoader;
 import com.kingja.qiang.injector.component.AppComponent;
-import com.kingja.supershapeview.view.SuperShapeLinearLayout;
+import com.kingja.qiang.util.AppUtil;
+import com.kingja.supershapeview.view.SuperShapeRelativeLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -42,11 +46,14 @@ public class OrderDetailActivity extends BaseTitleActivity implements OrderDetai
     TextView tvOrderOrderId;
     @BindView(R.id.tv_order_code)
     TextView tvOrderCode;
-    @BindView(R.id.wb_order_qcode)
-    WebView wbOrderQcode;
-    @BindView(R.id.ssll_qcode)
-    SuperShapeLinearLayout ssllQcode;
+    @BindView(R.id.vp_order)
+    ViewPager vpOrder;
+    @BindView(R.id.ssrl_qcode)
+    SuperShapeRelativeLayout ssrlQcode;
+    @BindView(R.id.ll_pointContainer)
+    LinearLayout llPointContainer;
     private String orderId;
+    private List<View> points = new ArrayList<>();
     @Inject
     OrderDetailPresenter orderDetailPresenter;
 
@@ -80,15 +87,7 @@ public class OrderDetailActivity extends BaseTitleActivity implements OrderDetai
 
     @Override
     protected void initData() {
-        wbOrderQcode.setWebViewClient(new WebViewClient(){
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                return false;
-            }
-        });
-        WebSettings webSettings = wbOrderQcode.getSettings();
-        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
-        webSettings.setUseWideViewPort(true);
+
     }
 
     @Override
@@ -121,7 +120,58 @@ public class OrderDetailActivity extends BaseTitleActivity implements OrderDetai
         tvOrderOrderId.setText(orderDetail.getOrderNo());
         tvOrderCode.setText(orderDetail.getStatus() == Status.OrderStatus.WAIT_USE.getCode() ? "出票中" : orderDetail
                 .getTicketcode());
-        ssllQcode.setVisibility(orderDetail.getQrcodeurl() == null ? View.GONE : View.VISIBLE);
-        wbOrderQcode.loadUrl(orderDetail.getQrcodeurl());
+        String qrcodeurl = orderDetail.getQrcodeurl();
+        ssrlQcode.setVisibility(TextUtils.isEmpty(qrcodeurl) ? View.GONE : View.VISIBLE);
+        if (!TextUtils.isEmpty(qrcodeurl)) {
+            String[] qcodes = qrcodeurl.split(",");
+            initDot(qcodes);
+            vpOrder.setAdapter(new QcodePagerAdapter(this, qcodes));
+            vpOrder.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                @Override
+                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                }
+
+                @Override
+                public void onPageSelected(int position) {
+                    if (qcodes.length < 2) {
+                        return;
+                    }
+                    for (int i = 0; i < points.size(); i++) {
+                        if (i == position) {
+                            points.get(i).setBackgroundResource(R.mipmap.ic_dot_action);
+                        } else {
+                            points.get(i).setBackgroundResource(R.mipmap.ic_dot_nor);
+                        }
+                    }
+                }
+
+                @Override
+                public void onPageScrollStateChanged(int state) {
+
+                }
+            });
+        }
+    }
+
+    private void initDot(String[] qcodes) {
+        if (qcodes.length < 2) {
+            return;
+        }
+        for (int i = 0; i < qcodes.length; i++) {
+            View view = new View(this);
+            if (i == 0) {
+                view.setBackgroundResource(R.mipmap.ic_dot_action);
+            } else {
+                view.setBackgroundResource(R.mipmap.ic_dot_nor);
+            }
+            points.add(view);
+        }
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(AppUtil.dp2px(10), AppUtil.dp2px(10));
+        layoutParams.setMargins(0, 0, AppUtil.dp2px(10), 0);
+        for (int i = 0; i < qcodes.length; i++) {
+            llPointContainer.addView(points.get(i), layoutParams);
+        }
+
     }
 }
